e74f1836af9374cbead05fb03d7be4bdefddb880
jonesga1 <gabriel.jones@ucdenver.edu>
2021-03-15 13:33:19 -0600

    Add conditionals to check for undefined

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -13,7 +13,10 @@ app.component('book-display', {
 		`<ul class = "col-sm-12 col-md-6 col-xl-4">
         <a class = "list-group-item list-group-item-action active" :href="this.bookObj.selfLink"> {{this.bookObj.volumeInfo.title}}</a>
         <li class = "list-group-item">{{this.bookObj.volumeInfo.authors}}</li>
-        <li class = "list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
+        <li class = "list-group-item">
+        	<p v-if = "this.bookObj.volumeInfo.publisher !== undefined">Publisher: {{this.bookObj.volumeInfo.publisher}}</p>
+        	<p v-if = "this.bookObj.volumeInfo.publishedDate !== undefined">Published Date: {{this.bookObj.volumeInfo.publishedDate}}</p>
+        </li>
         <li class = "list-group-item" v-if="this.bookObj.volumeInfo.imageLinks.medium !== undefined">
         	<img src="this.bookObj.volumeInfo.imageLinks.medium">
         </li>
